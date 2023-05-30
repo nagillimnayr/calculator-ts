3e33e692059beaea934881ef20753eb92ca210d9
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-29 19:52:47 -0700

    feat: add 'evaluateExpression' function

---
+++ src/math/expression/evaluateExp.ts
@@ -0,0 +1,12 @@
+import { Stack } from 'immutable';
+
+function evaluateExp(expression: string): string {
+  const result = '';
+
+  const numStack = Stack<number>();
+  const operatorStack = Stack<string>();
+
+  return result;
+}
+
+export default evaluateExp;
