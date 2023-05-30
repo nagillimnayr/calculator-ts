ed620a218d2d11d3c970fc5c4721c918deeb092c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-29 19:52:27 -0700

    feat: add 'Expression' class

---
+++ src/math/expression/Expression.ts
@@ -0,0 +1,38 @@
+import add from '../arithmetic/add';
+import divide from '../arithmetic/divide';
+import multiply from '../arithmetic/multiply';
+import subtract from '../arithmetic/subtract';
+
+/**
+ * @description The Expression class represents a mathematical
+ * expression with two operands and an operator
+ * @author Ryan Milligan
+ * @date 28/05/2023
+ * @class Expression
+ */
+class Expression {
+  operand1: number;
+  operand2: number;
+  operator: string;
+
+  constructor(operand1: number, operand2: number, operator: string) {
+    this.operand1 = operand1;
+    this.operand2 = operand2;
+    this.operator = operator;
+  }
+
+  evaluate(): number {
+    switch (this.operator) {
+      case '+':
+        return add(this.operand1, this.operand2);
+      case '-':
+        return subtract(this.operand1, this.operand2);
+      case '*':
+        return multiply(this.operand1, this.operand2);
+      case '/':
+        return divide(this.operand1, this.operand2);
+      default:
+        throw 'invalid operator!';
+    }
+  }
+}
