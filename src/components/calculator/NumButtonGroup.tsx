ca75bf7f4e78b21c6e08e37ccf0e7e5f5dbb9287
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-30 10:10:45 -0700

    feat: add numbers/ids to buttons

--- src/components/calculator/NumButtonGroup.tsx
+++ src/components/calculator/NumButtonGroup.tsx
@@ -4,24 +4,24 @@ export default function NumButtonGroup() {
   return (
     <div id="numButtonGroup" className="calculatorBtnGroup">
       <div id="numRow1">
-        <button></button>
-        <button></button>
-        <button></button>
+        <button id="btn7">7</button>
+        <button id="btn8">8</button>
+        <button id="btn9">9</button>
       </div>
       <div id="numRow2">
-        <button></button>
-        <button></button>
-        <button></button>
+        <button id="btn4">4</button>
+        <button id="btn5">5</button>
+        <button id="btn6">6</button>
       </div>
       <div id="numRow3">
-        <button></button>
-        <button></button>
-        <button></button>
+        <button id="btn1">1</button>
+        <button id="btn2">2</button>
+        <button id="btn3">3</button>
       </div>
       <div id="numRow4">
-        <button></button>
-        <button></button>
-        <button></button>
+        <button id="btn.">.</button>
+        <button id="btn0">0</button>
+        <button id="btn=">=</button>
       </div>
     </div>
   );
