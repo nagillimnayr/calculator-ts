0e94c56fa1c750f7af20b4c55169219e3a171e9c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-30 10:15:13 -0700

    chore: rename project and title to 'Calculator'

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'Calculator',
   entry: {
     app: './src/index.tsx',
   },
