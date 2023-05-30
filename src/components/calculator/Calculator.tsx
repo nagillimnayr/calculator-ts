43690f8ecf1555148fbe67d583039ef7e05937fd
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-30 10:13:39 -0700

    feat: add number buttons to Calculator component

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import NumButtonGroup from './NumButtonGroup';
 export default function Calculator() {
-  return <></>;
+  return (
+    <div id="calculator">
+      <div id="displayPanel"></div>
+      <div id="buttonPanel">
+        <NumButtonGroup />
+        <div id="operatorBtns"></div>
+      </div>
+    </div>
+  );
 }
