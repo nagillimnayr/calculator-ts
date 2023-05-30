0efcfa207bb8aa8b8e5794afcdac0b3bc37568c7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-30 10:08:08 -0700

    feat: seperate number buttons into own component

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -1,31 +1,4 @@
 import React from 'react';
 export default function Calculator() {
-  return (
-    <div id="calculator" className="calculator">
-      <div id="row1">
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-      </div>
-      <div id="row2">
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-      </div>
-      <div id="row3">
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-      </div>
-      <div id="row4">
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-      </div>
-    </div>
-  );
+  return <></>;
 }
---
+++ src/components/calculator/NumButtonGroup.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function NumButtonGroup() {
+  return (
+    <div id="numButtonGroup" className="calculatorBtnGroup">
+      <div id="numRow1">
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+      <div id="numRow2">
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+      <div id="numRow3">
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+      <div id="numRow4">
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+  );
+}
