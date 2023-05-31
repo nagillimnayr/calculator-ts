f4c19b0ae240adf3cf0a43f8c37c3062694c1f47
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-30 22:21:08 -0700

    refactor: move 'App' component into its own folder

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from '../calculator/Calculator';
+import './App.css';
+
+export default function App() {
+  return (
+    <div id="App">
+      <header id="App-header">
+        <h1>Calculator App</h1>
+        <hr />
+      </header>
+      <Calculator />
+    </div>
+  );
+}
