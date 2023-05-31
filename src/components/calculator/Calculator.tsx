26f6da138d4e59bd00d16e3f49b3915edd5b535f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-30 19:25:12 -0700

    feat: link buttons to handleClick

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { MouseEventHandler, useState } from 'react';
 import NumButtonGroup from './NumButtonGroup';
 import '../../style/calculator.css';
 import DisplayPanel from './DisplayPanel';
@@ -6,8 +6,9 @@ import DisplayPanel from './DisplayPanel';
 export default function Calculator() {
   const [expression, setExpression] = useState<string>('');
 
-  const handleClick = (str: string) => {
-    const newExpression = expression + str;
+  const handleClick: MouseEventHandler = (e: React.MouseEvent) => {
+    const btn = e.target as HTMLButtonElement;
+    const newExpression = expression + btn.textContent;
     setExpression(newExpression);
   };
   return (
--- src/components/calculator/DisplayPanel.tsx
+++ src/components/calculator/DisplayPanel.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import '../../style/calculator.css';
-import { format } from 'mathjs';
 
 export default function DisplayPanel({ displayStr }: { displayStr: string }) {
   return (
     <div id="displayPanel" className="displayPanel">
-      <p id="displayStr">{format(displayStr)}</p>
+      <p id="displayStr">{displayStr}</p>
     </div>
   );
 }
