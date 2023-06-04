c492fbd56bc38ad6c525d9eadfd19c68f1841b51
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-03 23:36:16 -0700

    fix: replace 'name' with 'id'

--- src/components/calculator/ButtonPanel.tsx
+++ src/components/calculator/ButtonPanel.tsx
@@ -89,115 +89,97 @@ export default function ButtonPanel({
             id="7"
             aria-label="seven"
             size="large"
+            variant="contained"
             startIcon={!usingBoxIcon ? <Numeric7 /> : <Numeric7Box />}
           ></Button>
         </Grid>
         <Grid item xs={1}>
-          <IconButton
+          <Button
             onClick={onClick}
-            name="8"
+            id="8"
             aria-label="eight"
             size="large"
-          >
-            {!usingBoxIcon ? <Numeric8 /> : <Numeric8Box />}
-          </IconButton>
+            variant="contained"
+            startIcon={!usingBoxIcon ? <Numeric8 /> : <Numeric8Box />}
+          ></Button>
         </Grid>
         <Grid item xs={1}>
-          <IconButton onClick={onClick} name="9" aria-label="nine" size="large">
-            {!usingBoxIcon ? <Numeric9 /> : <Numeric9Box />}
+          <IconButton onClick={onClick} id="9" aria-label="nine" size="large">
+            startIcon={!usingBoxIcon ? <Numeric9 /> : <Numeric9Box />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
           <IconButton
             onClick={onClick}
-            name="/"
+            id="/"
             aria-label="divide-by"
             size="large"
           >
-            {!usingBoxIcon ? <Division /> : <DivisionBox />}
+            startIcon={!usingBoxIcon ? <Division /> : <DivisionBox />}
           </IconButton>
         </Grid>
 
         <Grid item xs={1}>
-          <IconButton onClick={onClick} name="4" aria-label="four" size="large">
-            {!usingBoxIcon ? <Numeric4 /> : <Numeric4Box />}
+          <IconButton onClick={onClick} id="4" aria-label="four" size="large">
+            startIcon={!usingBoxIcon ? <Numeric4 /> : <Numeric4Box />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
-          <IconButton onClick={onClick} name="5" aria-label="five" size="large">
-            {!usingBoxIcon ? <Numeric5 /> : <Numeric5Box />}
+          <IconButton onClick={onClick} id="5" aria-label="five" size="large">
+            startIcon={!usingBoxIcon ? <Numeric5 /> : <Numeric5Box />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
-          <IconButton onClick={onClick} name="6" aria-label="six" size="large">
-            {!usingBoxIcon ? <Numeric6 /> : <Numeric6Box />}
+          <IconButton onClick={onClick} id="6" aria-label="six" size="large">
+            startIcon={!usingBoxIcon ? <Numeric6 /> : <Numeric6Box />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
-          <IconButton
-            onClick={onClick}
-            name="*"
-            aria-label="times"
-            size="large"
-          >
+          <IconButton onClick={onClick} id="*" aria-label="times" size="large">
+            startIcon=
             {!usingBoxIcon ? <Multiplication /> : <MultiplicationBox />}
           </IconButton>
         </Grid>
 
         <Grid item xs={1}>
-          <IconButton onClick={onClick} name="1" aria-label="one" size="large">
-            {!usingBoxIcon ? <Numeric1 /> : <Numeric1Box />}
+          <IconButton onClick={onClick} id="1" aria-label="one" size="large">
+            startIcon={!usingBoxIcon ? <Numeric1 /> : <Numeric1Box />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
-          <IconButton onClick={onClick} name="2" aria-label="two" size="large">
-            {!usingBoxIcon ? <Numeric2 /> : <Numeric2Box />}
+          <IconButton onClick={onClick} id="2" aria-label="two" size="large">
+            startIcon={!usingBoxIcon ? <Numeric2 /> : <Numeric2Box />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
-          <IconButton
-            onClick={onClick}
-            name="3"
-            aria-label="three"
-            size="large"
-          >
-            {!usingBoxIcon ? <Numeric3 /> : <Numeric3Box />}
+          <IconButton onClick={onClick} id="3" aria-label="three" size="large">
+            startIcon={!usingBoxIcon ? <Numeric3 /> : <Numeric3Box />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
-          <IconButton
-            onClick={onClick}
-            name="-"
-            aria-label="minus"
-            size="large"
-          >
-            {!usingBoxIcon ? <Minus /> : <MinusBox />}
+          <IconButton onClick={onClick} id="-" aria-label="minus" size="large">
+            startIcon={!usingBoxIcon ? <Minus /> : <MinusBox />}
           </IconButton>
         </Grid>
 
         <Grid item xs={1}>
-          <IconButton onClick={onClick} name="." aria-label="dot" size="large">
-            {!usingBoxIcon ? <CircleSmall /> : <Decimal />}
+          <IconButton onClick={onClick} id="." aria-label="dot" size="large">
+            startIcon={!usingBoxIcon ? <CircleSmall /> : <Decimal />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
-          <IconButton onClick={onClick} name="0" aria-label="zero" size="large">
-            {!usingBoxIcon ? <Numeric0 /> : <Numeric0Box />}
+          <IconButton onClick={onClick} id="0" aria-label="zero" size="large">
+            startIcon={!usingBoxIcon ? <Numeric0 /> : <Numeric0Box />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
-          <IconButton
-            onClick={onClick}
-            name="="
-            aria-label="equals"
-            size="large"
-          >
-            {!usingBoxIcon ? <Equal /> : <EqualBox />}
+          <IconButton onClick={onClick} id="=" aria-label="equals" size="large">
+            startIcon={!usingBoxIcon ? <Equal /> : <EqualBox />}
           </IconButton>
         </Grid>
         <Grid item xs={1}>
-          <IconButton onClick={onClick} name="+" aria-label="plus" size="large">
-            {!usingBoxIcon ? <Plus /> : <PlusBox />}
+          <IconButton onClick={onClick} id="+" aria-label="plus" size="large">
+            startIcon={!usingBoxIcon ? <Plus /> : <PlusBox />}
           </IconButton>
         </Grid>
       </Grid>
