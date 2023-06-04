a15df91e64e05bdda376fc34511a27271d990776
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-03 18:16:59 -0700

    docs: add comment to 'Calculator.tsx'

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -6,11 +6,13 @@ import DisplayPanel from './DisplayPanel';
 export default function Calculator() {
   const [expression, setExpression] = useState<string>('');
 
+  // Add character to expression string on button click
   const handleClick: MouseEventHandler = (e: React.MouseEvent) => {
     const btn = e.target as HTMLButtonElement;
     const newExpression = expression + btn.name;
     setExpression(newExpression);
   };
+
   return (
     <div id="calculator">
       <DisplayPanel displayStr={expression} />
