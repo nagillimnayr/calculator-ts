4dd599ef0b3e8db7145236b21e22dc2105fc464a
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-03 22:01:57 -0700

    feat: import material design icons into 'ButtonPanel' component

--- src/components/calculator/ButtonGroup.tsx
+++ src/components/calculator/ButtonGroup.tsx
@@ -2,6 +2,82 @@ import React, { MouseEventHandler } from 'react';
 import './Calculator.css';
 import CalculatorButton from './CalculatorButton';
 
+import AddIcon from '@mui/icons-material/Add'; /* + */
+import RemoveIcon from '@mui/icons-material/Remove'; /* - */
+import FunctionsIcon from '@mui/icons-material/Functions'; /* Sigma */
+import IsoIcon from '@mui/icons-material/Iso'; /* +/- */
+import PercentIcon from '@mui/icons-material/Percent'; /* % */
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh'; /* ! */
+import { SvgIcon, Grid, Button, ButtonGroup } from '@mui/material';
+import {
+  Calculator,
+  CalculatorVariant,
+  ChartBellCurve,
+  ChartBellCurveCumulative,
+  Close,
+  CloseBox,
+  CodeParentheses,
+  CodeParenthesesBox,
+  Division,
+  DivisionBox,
+  Equal,
+  EqualBox,
+  Exclamation,
+  Exponent,
+  ExponentBox,
+  FormatSuperscript,
+  Function,
+  FunctionVariant,
+  Infinity,
+  MathCos,
+  MathIntegral,
+  MathIntegralBox,
+  MathLog,
+  MathSin,
+  MathTan,
+  Minus,
+  MinusBox,
+  Multiplication,
+  MultiplicationBox,
+  Percent,
+  PercentBox,
+  Plus,
+  PlusBox,
+  PlusMinus,
+  PlusMinusBox,
+  PlusMinusVariant,
+  Pi,
+  PiBox,
+  Sigma,
+  SlashForward,
+  SlashForwardBox,
+  SquareRoot,
+  SquareRootBox,
+  Variable,
+  VariableBox,
+  Numeric,
+  Numeric0,
+  Numeric0Box,
+  Numeric1,
+  Numeric1Box,
+  Numeric2,
+  Numeric2Box,
+  Numeric3,
+  Numeric3Box,
+  Numeric4,
+  Numeric4Box,
+  Numeric5,
+  Numeric5Box,
+  Numeric6,
+  Numeric6Box,
+  Numeric7,
+  Numeric7Box,
+  Numeric8,
+  Numeric8Box,
+  Numeric9,
+  Numeric9Box,
+} from 'mdi-material-ui';
+
 export default function NumButtonGroup({
   onClick,
 }: {
