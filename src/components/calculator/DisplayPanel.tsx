77c7a4be213d07131709c046b6159cb7fce880b1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-04 17:25:33 -0700

    feat: improve the width of the DisplayPanel

--- src/components/calculator/DisplayPanel.tsx
+++ src/components/calculator/DisplayPanel.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import './Calculator.css';
+import {
+  Container,
+  TextField,
+  InputAdornment,
+  IconButton,
+} from '@mui/material';
+import { CloseCircle } from 'mdi-material-ui';
 
-export default function DisplayPanel({ displayStr }: { displayStr: string }) {
+export default function DisplayPanel({
+  expression,
+  setExpression,
+}: {
+  expression: string;
+  setExpression: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  function clearExpression(e: React.MouseEvent) {
+    const btn = e.currentTarget;
+    setExpression('');
+  }
+
   return (
-    <div id="displayPanel" className="displayPanel">
-      <p id="displayStr">{displayStr}</p>
-    </div>
+    <Container id="displayPanel" className="displayPanel" disableGutters>
+      <TextField
+        id="textField"
+        fullWidth
+        value={expression}
+        InputProps={{
+          readOnly: true,
+          endAdornment: (
+            <IconButton onClick={clearExpression}>
+              <CloseCircle />
+            </IconButton>
+          ),
+        }}
+        sx={{ fontSize: '2em' }}
+      ></TextField>
+    </Container>
   );
 }
